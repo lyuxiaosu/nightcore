85e86f052e5ec959e3ebe88312984fea75bc8276
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-04-27 11:34:25 -0600

    upload bypass_gw_curl.sh

--- bypass_gw_curl.sh
+++ bypass_gw_curl.sh
@@ -1,1 +1,2 @@
-curl -w "Total time: %{time_total}s\n" -o /dev/null -s -X POST http://10.10.1.1:8081/function/Foo http://10.10.1.1:8081/function/Foo 
+#curl -I -w "Total time: %{time_total}s\n" -o /dev/null -s -X POST http://10.10.1.1:8081/function/Foo http://10.10.1.1:8081/function/Foo 
+hey -disable-compression -disable-redirects -cpus 15 -z "20"s -c "30" -m POST "http://10.10.1.1:8081/function/Foo" 
