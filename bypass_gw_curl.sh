b0749925c73d2987ae855df10db7b01d90842f45
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-07-16 23:45:38 -0600

    update scripts

--- bypass_gw_curl.sh
+++ bypass_gw_curl.sh
@@ -1,4 +1,4 @@
 ulimit -n 655350
-curl -w "Total time: %{time_total}s TCP Connect time: %{time_connect}s\n" -o /dev/null -s -X POST http://10.10.1.1:8081/function/Foo http://10.10.1.1:8081/function/Foo 
+curl -I -w "Total time: %{time_total}s TCP Connect time: %{time_connect}s\n" -o /dev/null -s -X POST http://10.10.1.1:8081/function/Foo http://10.10.1.1:8081/function/Foo 
 #hey -disable-compression -disable-redirects -cpus 15 -z "20"s -c "60" -m POST "http://10.10.1.1:8081/function/Foo" 
 
